54549ec8cff0a53a2eb796087aa2152812e0e4fb
saifmd <saif.mohd786@gmail.com>
2026-05-17 14:03:48 +0530

    changes 6

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -34,12 +34,12 @@ const handleCheckoutShopping = (e) => {
     if (item.quantity > 1) {
       dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
     } else {
-      dispatch(removeItem({ name: item.name }));
+      dispatch(removeItem(item.name));
     }
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem({ name: item.name }));
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
